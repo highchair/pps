3a22a827b39332163a0f963b5c700f622e64ab46
K Belardinelli <k@kangabell.co>
2017-04-26 13:30:09 -0400

    desktop footer style

--- footer.php
+++ footer.php
@@ -4,39 +4,43 @@
 
       <div class="contact" itemscope itemtype="http://schema.org/Organization">
         <h1 class="logo-text" itemprop="name">Providence Preservation Society</h1>
-        <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+        <div class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
           <p><span itemprop="streetAddress">24 Meeting Street</span></p>
-          <p><span itemprop="addressLocality">Providence </span>,
-          <span itemprop="addressRegion">RI</span> (<a href="javascript:void(0)" itemprop="maps">Map</a>)</p>
+          <p>
+            <span itemprop="addressLocality">Providence </span>,
+            <span itemprop="addressRegion">RI</span> (<a href="javascript:void(0)" itemprop="maps">Map</a>)
+          </p>
         </div>
-        <p>ph: <span itemprop="telephone">(401) 831-7440</span></p>
-        <p><span itemprop="email">info@ppsri.org</span></p>
+        <p class="phone">ph: <span itemprop="telephone">(401) 831-7440</span></p>
+        <p class="email"><span itemprop="email">info@ppsri.org</span></p>
       </div>
 
-      
+    </div>
+
+    <div>
 
       <p class="social">
         <a href="http://facebook.com/pvdpreservation"><span class="icon-facebook"></span> facebook.com/pvdpreservation</a>
         <a href="http://twitter.com/pvdpreservation"><span class="icon-twitter"></span> twitter.com/pvdpreservation</a>
       </p>
 
+      <nav role="navigation">
+
+        <div class="secondary">
+          <a href="javascript:void(0)">About</a> <a href="javascript:void(0)">News</a> <a href="javascript:void(0)">Contact</a>
+        </div>
+
+        <div class="primary">
+            <a href="javascript:void(0)">Advocacy</a>
+            <a href="javascript:void(0)">Programs &amp; Events</a>
+            <a href="javascript:void(0)">Resources</a>
+            <a href="javascript:void(0)">Support</a>
+        </div>
+
+      </nav>
+
     </div>
 
-    <nav role="navigation">
-
-      <div class="secondary">
-        <a href="javascript:void(0)">About</a> <a href="javascript:void(0)">News</a> <a href="javascript:void(0)">Contact</a>
-      </div>
-
-      <div class="primary">
-          <div><a href="javascript:void(0)">Advocacy</a></div>
-          <div><a href="javascript:void(0)">Programs &amp; Events</a></div>
-          <div><a href="javascript:void(0)">Resources</a></div>
-          <div><a href="javascript:void(0)">Support</a></div>
-      </div>
-
-    </nav>
-  
   </footer>
 
   <?php wp_footer(); ?>
