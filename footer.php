2aee01dd6154b24134dada0eb4cc054a834a4cde
K Belardinelli <k@kangabell.co>
2017-05-09 16:53:35 -0400

    real navigation, and copyright info, in footer

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
       <div class="contact" itemscope itemtype="http://schema.org/Organization">
         <h1 class="logo-text" itemprop="name">Providence Preservation Society</h1>
         <div class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-          <p><span itemprop="streetAddress">24 Meeting Street</span></p>
+        <p><span itemprop="streetAddress">24 Meeting Street</span></p>
           <p>
             <span itemprop="addressLocality">Providence </span>,
             <span itemprop="addressRegion">RI</span> (<a href="javascript:void(0)" itemprop="maps">Map</a>)
@@ -34,15 +34,12 @@
 
       <nav role="navigation">
 
-        <div class="secondary">
-          <a href="javascript:void(0)">About</a> <a href="javascript:void(0)">News</a> <a href="javascript:void(0)">Contact</a>
+        <div class="secondary-nav">
+          <?php ppsri_secondary_nav(); ?>
         </div>
 
-        <div class="primary">
-            <a href="javascript:void(0)">Advocacy</a>
-            <a href="javascript:void(0)">Programs &amp; Events</a>
-            <a href="javascript:void(0)">Resources</a>
-            <a href="javascript:void(0)">Support</a>
+        <div class="primary-nav">
+            <?php ppsri_primary_nav(); ?>
         </div>
 
       </nav>
@@ -55,7 +52,7 @@
 
   <div class="subfooter">
     <p>
-      <span>&copy; 2017 Providence Preservation Society.</span>
+      <span>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</span>
       <span>All rights reserved.</span>
     </p>
   </div>
