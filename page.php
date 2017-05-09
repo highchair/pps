1aa3d91454dd6b4901b548374d13520c2c1ad40a
K Belardinelli <k@kangabell.co>
2017-05-09 17:27:43 -0400

    pages that are parents or children have a sidebar; pages that are standalone have no sidebar and are a little narrower.

--- page.php
+++ page.php
@@ -8,7 +8,13 @@
 		<?php bcn_display(); ?>
 	</nav>
 
-	<?php get_sidebar('page'); ?>
+	<?php 
+		// if the page is a child or parent, show sidebar navigation
+		$children = get_pages( array( 'child_of' => $post->ID ) );
+		if ( (is_page() && $post->post_parent) || (is_page() && count( $children ) > 0) ) {
+			get_sidebar('page'); 
+		}
+	?>
 
 	<article>
 
