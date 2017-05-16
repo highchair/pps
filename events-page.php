4613b1806f41d0b5d38d1610660a24485384306c
K Belardinelli <k@kangabell.co>
2017-05-16 17:55:00 -0400

    add working pagination to Events page (pagination partial used elsewhere is not working here...)

--- events-page.php
+++ events-page.php
@@ -21,14 +21,26 @@ Template Name: Events
       <h1>Upcoming Events</h1>
 
       <?php
-      $loop = new WP_Query( array( 'post_type'=>'events' ) );
-        while ($loop->have_posts()) :
-          $loop->the_post();
-          include 'partials/article.php';
-        endwhile; 
-        get_template_part('partials/pagination');
-      wp_reset_postdata();
+        $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+        $loop = new WP_Query( array('post_type' => 'events', 'paged' => $paged) );
       ?>
+
+      <?php if ( $loop->have_posts() ) : while ( $loop->have_posts() ) : $loop->the_post(); ?>
+        <?php get_template_part('partials/article'); ?>
+      <?php endwhile; ?>
+
+        <?php if ($loop->max_num_pages > 1) { ?>
+          <nav class="prev-next-posts">
+            <div class="prev-posts-link">
+              <?php echo get_next_posts_link( 'Older Events', $loop->max_num_pages ); ?>
+            </div>
+            <div class="next-posts-link">
+              <?php echo get_previous_posts_link( 'Newer Events' ); ?>
+            </div>
+          </nav>
+        <?php } ?>
+
+      <?php endif; ?>
 
     </article>
 
