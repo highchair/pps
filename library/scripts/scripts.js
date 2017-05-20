ffe5cef66acdd33d6ac2171bbe5b7a6fae299da2
K Belardinelli <k@kangabell.co>
2017-05-19 20:40:18 -0400

    refined desktop search field open/close animation & positioning

--- library/scripts/scripts.js
+++ library/scripts/scripts.js
@@ -17,11 +17,20 @@ jQuery(document).ready( function($) {
 
 	/*** SEARCH BAR (desktop only) ***/
 
+	// move search form to a different place on larger sizes
+	$(window).resize(function(){
+		if (Modernizr.mq('(min-width: 1024px)')) {
+			$('#searchform').prependTo('#header .desktop-search');
+		}
+	});
+
+	// open search input when link is clicked
 	$('#header .desktop-search').click( function() {
 
 		openSearch();
 
 	});
+
 
 	/*** KEYBOARD SHORTCUTS ***/
 
@@ -71,7 +80,6 @@ jQuery(document).ready( function($) {
 
 	function openSearch() {
 		$('#header nav').addClass('search-open');
-		$('#searchform').prependTo('#header .desktop-search');
 		$('#searchform input').focus();
 	}
 
