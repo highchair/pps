765cda1f5e2bbd2f19aab974222c12f88e665fa3
kangabell <k@kangabell.co>
2018-09-18 15:11:57 -0400

    remove duplicate js

--- library/scripts/waypoints.js
+++ library/scripts/waypoints.js
@@ -1,41 +1,5 @@
 jQuery(document).ready( function($) {
 
-	/*** NAV ***/
-
-	$('body').addClass('nav-closed');
-
-	$('.toggle-nav').click( function(e) {
-
-		// toggle nav
-		if ( $('body').hasClass('nav-open') ) {
-			closeNav();
-		} else {
-			openNav();
-		}
-
-	});
-
-	/*** KEYBOARD SHORTCUTS ***/
-
-	$(document).keydown(function(e) {
-		if ( e.keyCode == 27 ){ // esc key
-			closeNav();
-		}
-	});
-
-
-	/*** FUNCTIONS **/
-
-	function openNav() {
-		$('body').removeClass('nav-closed').addClass('nav-open');
-		$('html, body').animate({ scrollTop: $("#header nav").offset().top }, 500);
-	}
-
-	function closeNav() {
-		$('body').removeClass('nav-open').addClass('nav-closed');
-	}
-
-
   /*** WAYPOINTS ***/
   var sticky = new Waypoint.Sticky({
     element: $('#mainnav')[0]
