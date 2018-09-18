62f462eb179e94f8bedeb97007de01508f13572d
kangabell <k@kangabell.co>
2018-09-18 15:01:12 -0400

    fix indenting

--- library/scripts/equalize-and-expand.js
+++ library/scripts/equalize-and-expand.js
@@ -6,21 +6,22 @@ jQuery(document).ready( function($) {
 
     if (Modernizr.mq('(min-width: 1024px)')) {
 
-			// reset in case it's already been equalized
-			unEqualize('body.home section.hero > div');
-			unEqualize('body.home section.articles article a');
-			unEqualize('body.home section.block');
+		// reset in case it's already been equalized
+		unEqualize('body.home section.hero > div');
+		unEqualize('body.home section.articles article a');
+		unEqualize('body.home section.block');
 
-			// equalize
-      equalize('body.home section.hero > div');
-      equalize('body.home section.articles article a');
-      equalize('body.home section.block');
+		// equalize
+		equalize('body.home section.hero > div');
+		equalize('body.home section.articles article a');
+		equalize('body.home section.block');
 
     } else {
 
-			unEqualize('body.home section.hero > div');
-			unEqualize('body.home section.articles article a');
-			unEqualize('body.home section.block');			
+		unEqualize('body.home section.hero > div');
+		unEqualize('body.home section.articles article a');
+		unEqualize('body.home section.block');
+		
     }
   }).resize();   // Cause an initial widow.resize to occur
 
